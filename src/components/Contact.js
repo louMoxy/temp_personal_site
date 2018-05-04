8e510fc0da13ba8f51abee87b4f832319c75e904
unknown <louisemoxy@gmail.com>
2018-05-04 08:05:07 +0100

    Add bot field to form and update form name

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,11 +22,10 @@ export default class Contact extends React.Component {
         fetch("/", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact", ...this.state })
+          body: encode({ "form-name": "contactForm", ...this.state })
         })
           .then(() => this.setState({'thanks': 'Thanks for getting in contact.'}))
           .catch(error => alert(error));
-    
         e.preventDefault();
       };
     render() {
@@ -40,8 +39,12 @@ export default class Contact extends React.Component {
                 data-netlify="true"
                 name="contactForm"
                 onSubmit={this.handleSubmit}
+                data-netlify-honeypot="bot-field"
                 hidden={(this.state.thanks ? true : false)}
                 >
+                <label hidden>
+                    Don’t fill this out: <input name="bot-field" onChange={this.handleChange} />
+                </label>
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
                         <input type="text" name="name" id="name" onChange={this.handleChange} required/>
