835646c568ee3a518b8a2b3cb93ca59ec206262a
unknown <louisemoxy@gmail.com>
2018-05-04 07:14:06 +0100

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,14 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-118254237-2",
+        head: true,
+        exclude: ["/admin/*"]
+      },
+    }
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
